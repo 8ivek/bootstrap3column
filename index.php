ab880bb47eaff2c3a41dad4f84de34237bd00992
Bivek Joshi @ Home <bivek_j@yahoo.com>
2017-12-03 01:55:11 -0500

    Still working on bootstrap design

--- index.php
+++ index.php
@@ -41,17 +41,17 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">Project name</a>
+			<h1><a class="navbar-brand" href="#">HELPAID</a></h1>
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#">Dashboard</a></li>
-				<li><a href="#">Settings</a></li>
-				<li><a href="#">Profile</a></li>
+				<li><a href="#">Post Ad</a></li>
+				<li><a href="#">Register</a></li>
+				<li><a href="#">Login</a></li>
 				<li><a href="#">Help</a></li>
 			</ul>
 			<form class="navbar-form navbar-right">
-				<input type="text" class="form-control" placeholder="Search...">
+				<input type="text" class="form-control" placeholder="Search..." />
 			</form>
 		</div>
 	</div>
@@ -61,51 +61,29 @@
 	<div class="row">
 		<div class="col-sm-3 col-md-2 sidebar">
 			<ul class="nav nav-sidebar">
-				<li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-				<li><a href="#">Reports</a></li>
-				<li><a href="#">Analytics</a></li>
-				<li><a href="#">Export</a></li>
-			</ul>
-			<ul class="nav nav-sidebar">
-				<li><a href="">Nav item</a></li>
-				<li><a href="">Nav item again</a></li>
-				<li><a href="">One more nav</a></li>
-				<li><a href="">Another nav item</a></li>
-				<li><a href="">More navigation</a></li>
-			</ul>
-			<ul class="nav nav-sidebar">
-				<li><a href="">Nav item again</a></li>
-				<li><a href="">One more nav</a></li>
-				<li><a href="">Another nav item</a></li>
+				<li><h5>Service Categories</h5></li>
+				<li class="active"><a class="" href="childcare-nanny" title="Everything related to Childcare &amp; Nanny">Childcare &amp; Nanny (0)</a></li>
+				<li><a class="" href="classes-music-lessons" title="Everything related to Classes - Music Lessons">Classes - Music Lessons (0)</a></li>
+				<li><a class="" href="clearners-cleaning" title="Everything related to Cleaners &amp; Cleaning">Cleaners &amp; Cleaning(0)</a></li>
+				<li><a class="" href="coaching-tutors-languages" title="Everything related to Coaching, Tutors &amp; Languages">Coaching, Tutors &amp; Languages (0)</a></li>
+				<li><a class="" href="computer-website-design-development" title="Everything related to Computer - Website, Design &amp; Development">Computer - Website, Design &amp; Development (1)</a></li>
+				<li><a class="" href="computer-cellphone-mobile-sales-repair" title="Everything related to Computer - Sales &amp; Repairs">Computer, Cellphone &amp; Mobile - Sales &amp; Repair (0)</a></li>
+				<li><a class="" href="courses-education-training" title="Everyting related to Courses - Education &amp; Training">Courses - Education &amp; Training (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+				<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
 			</ul>
 		</div>
 		<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-			<h1 class="page-header">Dashboard</h1>
-
-			<div class="row placeholders">
-				<div class="col-xs-6 col-sm-3 placeholder">
-					<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-					<h4>Label</h4>
-					<span class="text-muted">Something else</span>
-				</div>
-				<div class="col-xs-6 col-sm-3 placeholder">
-					<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-					<h4>Label</h4>
-					<span class="text-muted">Something else</span>
-				</div>
-				<div class="col-xs-6 col-sm-3 placeholder">
-					<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-					<h4>Label</h4>
-					<span class="text-muted">Something else</span>
-				</div>
-				<div class="col-xs-6 col-sm-3 placeholder">
-					<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-					<h4>Label</h4>
-					<span class="text-muted">Something else</span>
-				</div>
-			</div>
-
-			<h2 class="sub-header">Section title</h2>
+			<h2 class="sub-header">Recent Ads</h2>
 			<div class="table-responsive">
 				<table class="table table-striped">
 					<thead>
---
+++ index2.php
@@ -0,0 +1,135 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title>Collapsible sidebar using Bootstrap 3</title>
+
+	<!-- Bootstrap CSS CDN -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Our Custom CSS -->
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+
+
+<div class="wrapper">
+	<!-- Sidebar Holder -->
+	<nav id="sidebar">
+		<div class="sidebar-header">
+			<h3>Service Categories</h3>
+		</div>
+
+		<ul class="list-unstyled components">
+			<li class="active"><a class="" href="childcare-nanny" title="Everything related to Childcare &amp; Nanny">Childcare &amp; Nanny (0)</a></li>
+			<li><a class="" href="classes-music-lessons" title="Everything related to Classes - Music Lessons">Classes - Music Lessons (0)</a></li>
+			<li><a class="" href="clearners-cleaning" title="Everything related to Cleaners &amp; Cleaning">Cleaners &amp; Cleaning(0)</a></li>
+			<li><a class="" href="coaching-tutors-languages" title="Everything related to Coaching, Tutors &amp; Languages">Coaching, Tutors &amp; Languages (0)</a></li>
+			<li><a class="" href="computer-website-design-development" title="Everything related to Computer - Website, Design &amp; Development">Computer - Website, Design &amp; Development (1)</a></li>
+			<li><a class="" href="computer-cellphone-mobile-sales-repair" title="Everything related to Computer - Sales &amp; Repairs">Computer, Cellphone &amp; Mobile - Sales &amp; Repair (0)</a></li>
+			<li><a class="" href="courses-education-training" title="Everyting related to Courses - Education &amp; Training">Courses - Education &amp; Training (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+			<li><a class="" href="electronics-repair" title="Everyting related to Electronics Repair">Electronics Repair (0)</a></li>
+		</ul>
+	</nav>
+
+	<!-- Page Content Holder -->
+	<div id="content">
+
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+
+				<div class="navbar-header">
+					<button type="button" id="sidebarCollapse" class="navbar-btn">
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+				</div>
+
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h1><a class="navbar-brand" href="#">HELPAID</a></h1>
+				</div>
+
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Post Ad</a></li>
+						<li><a href="#">Register</a></li>
+						<li><a href="#">Login</a></li>
+						<li><a href="#">Help</a></li>
+					</ul>
+					<form class="navbar-form navbar-right">
+						<input type="text" class="form-control" placeholder="Search..." />
+					</form>
+				</div>
+
+				<!--<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Post Ad</a></li>
+						<li><a href="#">Register</a></li>
+						<li><a href="#">Login</a></li>
+						<li><a href="#">Help</a></li>
+					</ul>
+				</div>-->
+			</div>
+		</nav>
+
+		<h2 class="sub-header">Recent Ads</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+		<div class="line"></div>
+
+		<h2>Lorem Ipsum Dolor</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+		<div class="line"></div>
+
+		<h2>Lorem Ipsum Dolor</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+		<div class="line"></div>
+
+		<h3>Lorem Ipsum Dolor</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
+
+
+
+
+
+<!-- jQuery CDN -->
+<script src="js/jquery.min.js"></script>
+<!-- Bootstrap Js CDN -->
+<script src="js/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+	$(document).ready(function () {
+		$('#sidebarCollapse').on('click', function () {
+			$('#sidebar').toggleClass('active');
+			$(this).toggleClass('active');
+		});
+	});
+</script>
+</body>
+</html>
