fa2e0e80de4cd271758f5a3afb5c219ba78531e3
Bivek Joshi @ Home <bivek_j@yahoo.com>
2017-12-03 23:01:13 -0500

    Sidebar positioning was done.

--- index2.php
+++ index2.php
@@ -80,7 +80,7 @@
 		<div class="row-fluid bg-primary text-white text-center" id="sidebarCollapse"><a href="javascript:;" class="navbar-btn">Service Categories</a></div>
 
 		<h2 class="sub-header">Recent Ads</h2>
-		<div class="row-fluid">
+		<div class="row-fluid clearfix">
 			<div class="col-md-12">
 				<div class="row">
 					<div class="col-md-8 col-xs-12">
@@ -139,9 +139,9 @@
 		<h3>Lorem Ipsum Dolor</h3>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	</div>
-</div>
 
-<div id="fade" class="hide"></div>
+	<div id="fade" class="hide"></div>
+</div>
 
 <!-- jQuery CDN -->
 <script src="js/jquery.min.js"></script>
@@ -157,7 +157,6 @@
 		});
 		$("#fade").click(function() {
 			$('#sidebar').toggleClass('active');
-			$(this).toggleClass('active');
 			$("#fade").toggleClass('hide');
 		})
 	});
